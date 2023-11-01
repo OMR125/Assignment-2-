ad8a954c3d64f333d67b5b78271528c1ec5c0cee
OMR125 <85144803+OMR125@users.noreply.github.com>
2023-11-02 01:26:35 +0200

    Create  main.cpp

---
+++ Task 2/main.cpp
@@ -0,0 +1,14 @@
+#include <bits./stdc++.h>
+#include "BigReal.h"
+
+using namespace std;
+
+int main() {
+    BigReal x("11.9000000000000000000000000000000001");
+    BigReal y("2333333333339.1134322222222292");
+    BigReal z = x+y;
+    cout << (x == y) << "\n";
+    cout << (x > y) << "\n";
+    cout << (x < y) << "\n";
+    cout << z << "\n";
+}
