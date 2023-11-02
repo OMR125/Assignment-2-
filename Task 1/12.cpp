f9274891b575faaecc43046735c43ec446fe1e4e
OMR125 <85144803+OMR125@users.noreply.github.com>
2023-11-02 20:55:24 +0200

    Update 12.cpp

--- Task 1/12.cpp
+++ Task 1/12.cpp
@@ -1,48 +1,38 @@
-#include <iostream>
+#include <bits./stdc++.h>
+
 using namespace std;
-#include<vector>
-#include <fstream>
+
 int main() {
-    string s[]={"payment","invoice","statement","copy","verify","action","urgent","View","file","bank","money","suspicious","New","Notification","Expire","company","taxes","credit","pay","due","username","please","send","president","ceo","stop","investment","crypto","business","scam"};
-    vector<pair<string,int>>check;
-    pair<string,int>pair1;
-    for (int i = 0; i <30; ++i) {
-        pair1= make_pair(s[i],0);
-        check.push_back(pair1);
-    }
+    string s[] = {"payment", "invoice", "statement", "copy", "verify", "action", "urgent", "view", "file", "bank",
+                  "money", "suspicious", "new", "notification", "expire", "company", "taxes", "credit", "pay", "due",
+                  "username", "please", "send", "president", "ceo", "stop", "investment", "crypto", "business", "scam"};
+    vector <pair<string, int>> check;
+    for (int i = 0; i < 30; ++i)
+        check.push_back({s[i], 0});
     ifstream email("email.txt");
-    vector<string>store;
     string word;
-    while(email>>word){
-        store.push_back(word);
+    set<char> st = {',', '.', '?', '!', ';', '-', ':'};
+    while (email >> word) {
+        while (st.count(word.back()))
+            word.pop_back();
+        transform(word.begin(), word.end(), word.begin(), ::tolower);
+        for (int j = 0; j < 30; ++j)
+            check[j].second += (word == check[j].first);
     }
-    for (int i = 0; i <store.size() ; ++i) {
-        for (int j = 0; j <30 ; ++j) {
-            if (store[i]==check[j].first) {
-                check[j].second++;
-                cout <<" "<<check[j].first << " " << check[j].second << " ";
-                if (j < 10) {
-                    cout << (check[j].second) * 3;
-                } else if (j < 20) {
-                    cout << (check[j].second) * 2;
-                } else {
-                    cout << check[j].second << " ";
-                }
-            }
-        }
-        }
-    int sum=0;
+    int sum = 0;
     for (int i = 0; i < 30; ++i) {
-        if(i<10) {
-            sum+=((check[i].second) * 3);
+        if (i < 10) {
+            cout << check[i].first << " " << check[i].second << " " << check[i].second * 3 << "\n";
+            sum += (check[i].second * 3);
+        } else if (i < 20) {
+            cout << check[i].first << " " << check[i].second << " " << check[i].second * 2 << "\n";
+            sum += (check[i].second * 2);
+        } else {
+            cout << check[i].first << " " << check[i].second << " " << check[i].second << "\n";
+            sum += check[i].second;
         }
-        else if(i<20) {
-            sum+=((check[i].second) * 2);
-        }
-        else
-            sum+=(check[i].second);
     }
-    cout<<" "<<sum;
+    cout << sum;
 }
 
 
