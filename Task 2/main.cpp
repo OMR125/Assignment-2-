c4887ef887e9940507bfcbbde86df73f3c6678a9
OMR125 <85144803+OMR125@users.noreply.github.com>
2023-11-02 02:03:00 +0200

    Update main.cpp

--- Task 2/main.cpp
+++ Task 2/main.cpp
@@ -6,9 +6,11 @@ using namespace std;
 int main() {
     BigReal x("11.9000000000000000000000000000000001");
     BigReal y("2333333333339.1134322222222292");
-    BigReal z = x+y;
+    BigReal z = x + y;
     cout << (x == y) << "\n";
     cout << (x > y) << "\n";
     cout << (x < y) << "\n";
+    cout << z << "\n";
+    z += BigReal("5");
     cout << z << "\n";
 }
