0b18f7fa76eca59fc11e767fdb2a590184b5015a
OMR125 <85144803+OMR125@users.noreply.github.com>
2023-11-05 17:41:17 +0200

    Update main.cpp

--- Task 2/main.cpp
+++ Task 2/main.cpp
@@ -22,12 +22,12 @@ using namespace std;
 int main() {
     BigReal a("0.1243546000");
     BigReal b;
-    b.setNum("-999999999999999999999999999999.99999999999");
+    b.setNum("-999999999.99999999999");
     BigReal c = b;
     BigReal d = c + a;
     BigReal e = a - d;
     e += BigReal("-503429.7832");
     d -= BigReal("-91213.00000003");
-    cout << --e << " " << ++d << "\n";
+    cout << e << " " << d << "\n";
     cout << (e > d) << " " << (a == b) << " " << (c != c) << "\n";
 }
