f5c8dda216a4fbb9ac019be8a5cd74e418893c7e
OMR125 <85144803+OMR125@users.noreply.github.com>
2023-11-14 00:30:27 +0200

    Create machine.h

---
+++ Task 3/machine.h
@@ -0,0 +1,19 @@
+#ifndef VOLESIMULATER_MACHINE_H
+#define VOLESIMULATER_MACHINE_H
+
+#include "memory.h"
+#include "registers.h"
+#include "instructions.h"
+#include "Hexa.h"
+
+class machine : public Hexa {
+public:
+    memory a;
+    instructions b;
+    registers c;
+
+    void work();
+};
+
+
+#endif
