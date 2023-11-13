9af9a52ed1bf122a6256464354aad275f23d6c84
OMR125 <85144803+OMR125@users.noreply.github.com>
2023-11-14 00:25:32 +0200

    Update and rename memory to memory.h

---
+++ Task 3 /memory.h
@@ -0,0 +1,16 @@
+#include <bits./stdc++.h>
+
+#ifndef VOLESIMULATER_MEMORY_H
+#define VOLESIMULATER_MEMORY_H
+using namespace std;
+
+class memory {
+public:
+    string cell[256];
+
+    void fetch(string filename);
+
+    void fill();
+};
+
+#endif
