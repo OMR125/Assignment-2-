93ceb6ef176396f1c187933fdcbb63218cbea512
Medhatahmedabdelkarim <147209363+Medhatahmedabdelkarim@users.noreply.github.com>
2023-11-14 15:13:37 +0200

    Update instructions.cpp

--- Task 3/instructions.cpp
+++ Task 3/instructions.cpp
@@ -23,11 +23,12 @@ void instructions::MovePattern(string reg[], string s) {
 void instructions::Add(string reg[], string s) {
     int carry = 0;
     string result;
+    bool check=true;
     int bit1 = HexToInt(reg[HexToInt(string(1, s[2]))]);
     int bit2 = HexToInt(reg[HexToInt(string(1, s[3]))]);
     bitset<8>bitset1(bit1);
     bitset<8>bitset2(bit2);
-    for (int i = 0; i < 8; ++i) {
+    for (int i = 0; i <8; ++i) {
         int bit_a = bitset1[i];
         int bit_b = bitset2[i];
         int sum = bit_a + bit_b + carry;
@@ -35,20 +36,12 @@ void instructions::Add(string reg[], string s) {
         carry = sum / 2;
         result.push_back(bit + '0');
     }
-    if (carry == 1) {
-        result.push_back('1');
+    if(carry==1){
+        check=false;
     }
-    bool check = true;
-    if (result.back() == '1' && result.length() == 9) {
-        result.pop_back();
-        check = false;
-    }
-    reverse(result.begin(), result.end());
+    reverse(result.begin(),result.end());
     bitset<8>bitset3(result);
-    int result2=bitset3.to_ulong();
-    if (!check) {
-        reg[HexToInt(string(1, s[1]))] += "-";
-        reg[HexToInt(string(1, s[1]))] += result2;
-    } else
-        reg[HexToInt(string(1, s[1]))] = result2;
+    int result2= bitset3.to_ulong();
+    reg[HexToInt(string(1, s[1]))] = IntToHex(result2);
+
 }
