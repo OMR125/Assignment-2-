e2e75b220957b0012dd8143b99c7a5491256c458
OMR125 <85144803+OMR125@users.noreply.github.com>
2023-11-15 20:03:47 +0200

    Update main.cpp

--- Task 3/main.cpp
+++ Task 3/main.cpp
@@ -1,12 +1,8 @@
-#include <bits./stdc++.h>
-#include "memory.h"
-#include "registers.h"
 #include "machine.h"
-#include "instructions.h"
 
 using namespace std;
 
 int main() {
     machine x;
-    x.work();
+    x.run();
 }
