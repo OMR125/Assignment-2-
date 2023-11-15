0669e4aeec26dc77ac004b7307b19f3815f8108b
OMR125 <85144803+OMR125@users.noreply.github.com>
2023-11-15 20:24:15 +0200

    Update main.cpp

--- Task 3/main.cpp
+++ Task 3/main.cpp
@@ -1,8 +1,19 @@
+#include <bits./stdc++.h>
 #include "machine.h"
 
 using namespace std;
 
 int main() {
-    machine x;
-    x.run();
+    int n;
+    while (true) {
+        cout << "NOTE: Registers and memory will be printed after the execution of the file:\n";
+        cout << "Press 1 to start a new file or 0 to halt the program:\n";
+        cin >> n;
+        if (!n)
+            break;
+        else if (n == 1) {
+            machine x;
+            x.run();
+        } else cout << "INVALID OPTION\n";
+    }
 }
