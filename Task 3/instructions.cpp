955ad569997eba73ec82d96b9bea53783ec5167d
OMR125 <85144803+OMR125@users.noreply.github.com>
2023-11-15 20:01:26 +0200

    Update instructions.cpp

--- Task 3/instructions.cpp
+++ Task 3/instructions.cpp
@@ -12,7 +12,7 @@ void instructions::LoadPattern(string reg[], string s) {
 }
 
 void instructions::StorePattern(string reg[], string cell[], string s) {
-    if (s.substr(2) == "00") cout << "0x" << reg[HexToInt(string(1, s[1]))] << "\n";
+    if (s.substr(2) == "00") cout << "Screen: 0x" << reg[HexToInt(string(1, s[1]))] << "\n";
     else cell[HexToInt(s.substr(2))] = reg[HexToInt(string(1, s[1]))];
 }
 
@@ -23,12 +23,12 @@ void instructions::MovePattern(string reg[], string s) {
 void instructions::Add(string reg[], string s) {
     int carry = 0;
     string result;
-    bool check=true;
+    bool check = true;
     int bit1 = HexToInt(reg[HexToInt(string(1, s[2]))]);
     int bit2 = HexToInt(reg[HexToInt(string(1, s[3]))]);
-    bitset<8>bitset1(bit1);
-    bitset<8>bitset2(bit2);
-    for (int i = 0; i <8; ++i) {
+    bitset<8> bitset1(bit1);
+    bitset<8> bitset2(bit2);
+    for (int i = 0; i < 8; ++i) {
         int bit_a = bitset1[i];
         int bit_b = bitset2[i];
         int sum = bit_a + bit_b + carry;
@@ -36,19 +36,17 @@ void instructions::Add(string reg[], string s) {
         carry = sum / 2;
         result.push_back(bit + '0');
     }
-    if(result[0]==1){
-        check=false;
+    if (result[0] == 1) {
+        check = false;
     }
-    reverse(result.begin(),result.end());
-    bitset<8>bitset3(result);
+    reverse(result.begin(), result.end());
+    bitset<8> bitset3(result);
     int result2;
-    if(!check) {
+    if (!check) {
         result2 = bitset3.to_ulong();
         result2 -= 16;
-    }
-    else
+    } else
         result2 = bitset3.to_ulong();
 
     reg[HexToInt(string(1, s[1]))] = IntToHex(result2);
-
 }
